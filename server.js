957be1b1217ff2da4c0a292a7e8052a2918efe32
Nick Balestra <nbalestra@opentable.com>
2016-09-14 21:13:20 +0100

    basic express server handling jasontokens authentication

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(require('./routes/auth-routes'))
+
+app.listen(port, () =>
+	console.log(`👂 Server listening on port ${port}`)
+)
