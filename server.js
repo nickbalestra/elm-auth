8e7c268cbffe632fc1ac126c08ed9b209a71bab6
Nick Balestra <nbalestra@opentable.com>
2016-09-16 02:22:25 +0100

    dependencies

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-
 const app = express()
-const port = process.env.PORT || 3001
+const { passport } = require('./config')
+const { secret, port } = require('./config')
+const routes = require('./routes')
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
-app.use(require('./routes/auth-routes'))
+// App-level middleware for logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret, resave: true, saveUninitialized: true }));
 
+// Init Passport and restore auth state, if any, from the session.
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Routes.
+// require('./routes')(app)
+app.get('/', routes.home)
+app.get('/login/twitter', routes.login.twitter)
+app.get('/login/twitter/return', routes.login.twiiterCallbackURL)
+
+// Inorite...
 app.listen(port, () =>
-	console.log(`👂 Server listening on port ${port}`)
+  console.log('👂 Server listening on port ${port} ')
 )
