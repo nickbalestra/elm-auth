a23ff7f102b6e4278850404297224d4b9d9b70ff
Nick Balestra <nbalestra@opentable.com>
2016-09-17 20:37:43 +0100

    cleanups

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const config = require('./config')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = config.port
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
