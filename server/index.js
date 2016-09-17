b28492e7f85775b655128e17cb98cb9bef1fd662
Nick Balestra <nbalestra@opentable.com>
2016-09-17 11:34:26 +0100

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
-const app = express()
-const passport = require('./server/passport')
-const { cookie, port, secret } = require('./server/config')
-const routes = require('./server/routes')
+const passport = require('./passport')
+const { cookie, port, secret } = require('./config')
+const routes = require('./routes')
 const session = require('express-session')
 const RDBStore = require('session-rethinkdb')(session)
 
+const app = module.exports = express()
+
 // rethinkDB connection and session store => db: 'elmAuth', table: 'session'
-const r = require('rethinkdbdash')({ db: 'elmAuth' })
+const r = require('rethinkdbdash')({ db: process.env.DB_NAME })
 const store = new RDBStore(r,  { browserSessionsMaxAge: 5000 })
 
 // App-level middlewares for logging, parsing, and session handling.
@@ -24,6 +27,8 @@ app.get('/login/twitter', routes.login.twitter)
 app.get('/login/twitter/return', routes.login.twiiterCallbackURL)
 
 // Inorite...
-app.listen(port, () =>
-  console.log(`👂 Server listening on port ${port}`)
-)
+if (!module.parent) {
+  app.listen(port, () =>
+    console.log(`⚡  Express started on port ${port}`)
+  )
+}
